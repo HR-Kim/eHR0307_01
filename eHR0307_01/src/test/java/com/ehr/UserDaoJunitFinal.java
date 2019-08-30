789a79b014439cffea118787f4279c9158597b30
HR.KIM <jamesol@paran.com>
2019-08-30 17:09:06 +0900

    commit

--- eHR0307_01/src/test/java/com/ehr/UserDaoJunitFinal.java
+++ eHR0307_01/src/test/java/com/ehr/UserDaoJunitFinal.java
@@ -26,7 +26,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class UserDaoJunitFinal {
 	
 	private Logger LOG = Logger.getLogger(UserDaoJunitFinal.class);
-	
+	  
 	@Autowired
 	ApplicationContext context;
 	
